06cb20c1edc81b08015a26a137fcafcbd02bf38f
Colin Thompson <thompsoncolinm@gmail.com>
2018-10-18 12:29:29 -0400

    made mario theme

--- src/index.js
+++ src/index.js
@@ -1,23 +1,71 @@
 import _ from 'lodash';
 import './style.css';
-import Icon from './icon.png';import Data from './data.xml';
+import Icon from './icon.png';
+import Data from './data.xml';
 
-function component() {
-  var element = document.createElement('div');
+// Scope and Closures
+
+function component1() {
+  // Element variable is called in both component1() and component2() functions
+  var element = document.createElement('div'); // First "scoped" element variable
+  var element2 = document.createElement('div'); 
+  // Second element variable in same scope. This needs to be named 'element2'
 
   // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+  element2.innerHTML = _.join(['Hello', 'webpack', '. This is the component1 function :D'], ' ');
+  element2.classList.add('hello');
+
+  element.appendChild(element2);
+
+  element.classList.add('background');
 
   // Add the image to our existing div.
   var myIcon = new Image();
   myIcon.src = Icon;
 
-  element.appendChild(myIcon);
+  element2.appendChild(myIcon);
 
-    console.log(Data);
+  // Push xml document to the console
+  console.log(Data);
 
   return element;
-}
+} // Component1 Function
 
-document.body.appendChild(component());
+function component2(globalElement) {
+  // Element variable is called in both component1() and component2() functions
+  var element = document.createElement('div'); // Second "scoped" element variable
+
+  /*
+   Lodash is used along with Grave accents `` to produce HTML to be displayed
+   Grave accents `` allow the html to be formatted for easier viewing and characters 
+   don't need to be escaped when using eiter single or double quotes
+  */
+   element.innerHTML = _.join([`
+    <p>This is the component2 function<p>
+    <p>It also uses the "element" variable<p>
+    <p>This is in it's own scope<p>
+    <p>You can insert a global element like this:<p>
+  `+ globalElement], ' ');
+  element.classList.add('scope');
+  return element;
+} // Component2 Function
+
+var globalStyle=`
+background-color: #d4fffc;
+color: #65008a;
+border: 1px solid #4ec1e0;
+width: 70%;
+max-width: 320px;
+text-align: center;
+margin: auto;
+`
+
+var element = `
+<div class="global-element" style="`+globalStyle+`">
+This variable is called globally. It's also using inline styles.
+</div>
+`; // This is my 'global' element variable 
+
+// Both functions are appended to the project's 'body' tag
+document.body.appendChild(component1());
+document.body.appendChild(component2(element));
